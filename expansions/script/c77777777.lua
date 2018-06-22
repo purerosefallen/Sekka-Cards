c6d58865a2636a9a5450f1440e84fe98a7fcb5bd
purerosefallen <78877@qq.com>
2018-06-22 14:52:43 +0800

    first

---
+++ expansions/script/c77777777.lua
@@ -0,0 +1,7 @@
+--Paganini Caprice Number 24
+local m=77777777
+local cm=_G["c"..m]
+xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
+function cm.initial_effect(c)
+	
+end
